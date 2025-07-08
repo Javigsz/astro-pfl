a3bf3c384cf0c3a1a8a0453f5109e1f5c1ea485c
Javigsz <javiguerrerosanchez@gmail.com>
2025-07-08 14:00:36 +0200

    Add 2 more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,10 +10,30 @@ export const projects = [
     source: "https://github.com/javigsz/managementBoard",
   },
   {
+    id: 5,
+    title: "OrangeSky",
+    description:
+      "OrangeSky is a web social media application similar to twitter. It allows you to create a profile, post tweets and interact with other users.",
+    image: "/images/Project-5.png",
+    tags: ["NextJs", "Tailwind", "Firebase"],
+    link: "orange-sky.vercel.app",
+    source: "https://github.com/javigsz/orangeSky",
+  },
+  {
+    id: 6,
+    title: "Local Weather",
+    description:
+      "Local Weather is a web application that allows you to get the weather of a specific location. It uses the OpenWeather API.",
+    image: "/images/Project-6.png",
+    tags: ["VueJs", "Tailwind"],
+    link: "localweathertracker.netlify.app",
+    source: "https://github.com/Javigsz/weather-app",
+  },
+  {
     id: 2,
     title: "PDI Web",
     description:
-      "PDI Web is a web application that allows you to keep track of your actual and future entertainment.",
+      "PDI Web is a web application that allows you to keep track of your actual and future entertainment like movies or books.",
     image: "/images/Project-2.png",
     tags: ["React", "Tailwind", "Expressjs", "Nodejs", "MongoDB"],
     link: "https://pordondeiba-pdi.netlify.app",
